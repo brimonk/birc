39449f12c9374c7cdb280609dcd1bf4e4fc7d44b
Brian Chrzanowski <chrzanowskitest@gmail.com>
2024-08-08 17:09:20 -0400

    we added monsters :)

---
+++ monsters.h
@@ -0,0 +1,10 @@
+#ifndef MONSTERS_H
+#define MONSTERS_H
+
+// MONSTER_GetRandomMonsterName: return a random string from the monster list table.
+char *MONSTER_GetRandomMonsterName(void);
+
+// MONSTER_GetMonsterQuestAction: returns a random monster quest action.
+char *MONSTER_GetMonsterQuestAction(void);
+
+#endif // MONSTERS_H
--- rpg_quest.c
+++ rpg_quest.c
@@ -2,6 +2,7 @@
 #include "irc.h"
 #include "rpg.h"
 #include "rpg_quest.h"
+#include "monsters.h"
 
 // irc_botcmd_quest_completed: completes the quest for the user
 void *irc_botcmd_quest_completed(void *ptr)
@@ -52,143 +53,11 @@ void *irc_botcmd_quest_completed(void *ptr)
 	return NULL;
 }
 
-// NOTE All of these quests are basically madlibs styled strings of the following format:
-//     <PLAYER> goes on a quest in <QUEST PLACE> to <QUEST GOAL>. They <SUCCEED/FAIL>, and gain
-//     <XP>XP and <GP>GP.
-
-struct {
-	char *place;
-	char *goal[4];
-} QuestMetadata[] = {
-	{
-		"the arctic",
-		{
-			"find and rescue some lost adventurers",
-			"bring fuel to a distant outpost",
-			"melt a frost giant",
-			"put an end to an ice dragon",
-		}
-	},
-	{
-		"the artisan district of town",
-		{
-			"locate a local thief",
-			"clear out a group of local drunkards",
-			"find where a kobold hideout is",
-			"put at stop to the Bywater Barons",
-		}
-	},
-#if 0
-	{
-		"the castle district",
-		{
-
-		}
-	},
-	{
-		"a local cave",
-		{
-		}
-	},
-	{
-		"some deep tunnels",
-		{
-		}
-	},
-	{
-		"the far away desert",
-		{
-			"find the mysterious, wandering merchant",
-		}
-	},
-	{
-		"the local forest",
-		{
-		}
-	},
-	{
-		"close by grasslands",
-		{
-		}
-	},
-	{
-		"the high district",
-		{
-		}
-	},
-	{
-		"a far away jungle",
-		{
-		}
-	},
-	{
-		"the low district",
-		{
-			"find where a newly born rat king is hiding",
-		}
-	},
-	{
-		"the market",
-		{
-		}
-	},
-	{
-		"a distant mountain",
-		{
-		}
-	},
-	{
-		"the ocean off the coast",
-		{
-		}
-	},
-	{
-		"a local river basin",
-		{
-		}
-	},
-	{
-		"near by ruins",
-		{
-		}
-	},
-	{
-		"the slums",
-		{
-		}
-	},
-	{
-		"a near by swamp",
-		{
-		}
-	},
-	{
-		"the temple district",
-		{
-		}
-	},
-	{
-		"a near by tomb",
-		{
-		}
-	},
-	{
-		"the university district",
-		{
-		}
-	},
-#endif
-};
-
 // rpg_quest_generate: writes a quest into the buffer 's'
 void rpg_quest_generate(char *s, size_t slen)
 {
-	i32 place, goal;
-
-	place = rand() % ARRSIZE(QuestMetadata);
-	goal = rand() % ARRSIZE(QuestMetadata[0].goal);
-
-	snprintf(s, slen, "%%s goes on a quest in %s to %s...",
-		QuestMetadata[place].place, QuestMetadata[place].goal[goal]
+	snprintf(s, slen, "%%s goes on a quest to %s the/a %s...",
+		MONSTER_GetMonsterQuestAction(),
+		MONSTER_GetRandomMonsterName()
 	);
 }
